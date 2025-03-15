50ea3489995d71746cb57ea760cd5ac65019f83f
Degelo-hub <julia.a.degelo@gmail.com>
2025-03-15 11:14:34 +0100

    front end m6

---
+++ script.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# MNER Tagger App")   
+
+st.markdown("This is a MNER tagging app that uses the WikiNeural model to tag texts in English, French, German and Italian.")
